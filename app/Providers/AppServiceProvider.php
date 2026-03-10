02c22f775afb7291be22b79cf58821cdc715b7e9
Alif Yasir Athoriq <alifthoriq3105@gmail.com>
2026-03-10 11:25:29 +0700

    |MQ| Fix CSS loading: Force HTTPS & Update URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -18,7 +18,10 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        //
+{
+    // Paksa HTTPS jika di server Production
+    if (config('app.env') === 'production') {
+        URL::forceScheme('https');
     }
+}
 }
